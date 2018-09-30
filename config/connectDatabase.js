fec92493bf1f36e2fef8f3572de9d24941aa54c9
ducnm98 <ducnm.john98@gmail.com>
2018-09-30 10:51:35 +0700

    Edit connection mongodb

--- config/connectDatabase.js
+++ config/connectDatabase.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/system-hsv-lan4", { useNewUrlParser: true });
+mongoose.connect("mongodb://systemhsv:systemHSV123@ds115613.mlab.com:15613/system-hsv-lan4", { useNewUrlParser: true });
