48cf1057055c017236de8675db66b6530efcdb5a
ducnm98 <ducnm.john98@gmail.com>
2018-09-30 23:19:44 +0700

    a

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,7 +1,7 @@
 var router = require("express").Router();
 
 router.use("/delegates", require("./delegate"));
-router.use("/vote", require("./votes"));
+router.use("/votes", require("./votes"));
 router.use("/", require("./adminPage"));
 
 module.exports = router;
