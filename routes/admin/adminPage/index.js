0455e0d8b801cffa12e87ff359fe87a8738590cd
ducnm98 <ducnm.john98@gmail.com>
2018-10-01 22:40:56 +0700

    Fix error when test socket

--- routes/admin/adminPage/index.js
+++ routes/admin/adminPage/index.js
@@ -2,10 +2,6 @@ var router = require("express").Router();
 var { redirectToLogin } = require("../../../services/returnToUsers");
 var { TYPE_OF_ADMIN } = require("../../constants");
 var { checkPermission } = require('../../../services/checkPermission');
-var mongoose = require('mongoose');
-var bcryptjs = require('bcryptjs');
-var { updateNumberOfDelegates, delegatesIn, delegatesOut } = require('../../../services/socket');
-
 
 router.get("/", (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -18,34 +14,6 @@ router.get("/", (req, res, next) => {
     return redirectToLogin(res);
   }
 });
-
-router.get('/test/:admin/:pass', (req, res) => {
-  mongoose.model('delegates').findOne({ email: req.params.admin }, (err, result) => {
-    console.log(result)
-    bcryptjs.compare(req.params.pass, result.password, (err, isMatch) => {
-      if (isMatch) {
-        return res.render("admin/delegates/getCode", { roles: result.roles });
-      }
-    })
-  })
-});
-
-router.get('/test/:id', (req, res) => {
-  let update = {
-    $push: {
-      delegates: {
-        delegateName: 'a',
-        gender: true,
-      }
-    }
-  }
-  mongoose.model('attendance').findOneAndUpdate({ _id: req.params.id }, update, {new: true}, (err, result) => {
-    if (result) {
-      updateNumberOfDelegates(result);
-      return res.send(result)
-    }
-  })
-})
 
 require("./login")(router);
 
