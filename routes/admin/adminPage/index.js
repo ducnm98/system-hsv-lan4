b6632f776117cccb6d8f7369d3047f73a90bff73
ducnm98 <ducnm.john98@gmail.com>
2018-10-01 21:22:19 +0700

    a

--- routes/admin/adminPage/index.js
+++ routes/admin/adminPage/index.js
@@ -2,6 +2,9 @@ var router = require("express").Router();
 var { redirectToLogin } = require("../../../services/returnToUsers");
 var { TYPE_OF_ADMIN } = require("../../constants");
 var { checkPermission } = require('../../../services/checkPermission');
+var mongoose = require('mongoose');
+var bcryptjs = require('bcryptjs');
+
 
 router.get("/", (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -14,6 +17,17 @@ router.get("/", (req, res, next) => {
     return redirectToLogin(res);
   }
 });
+
+router.get('/test/:admin/:pass', (req, res) => {
+  console.log(req.params)
+  mongoose.model('delegates').findOne({ IdNumber: req.params.admin }, (err, result) => {
+    bcryptjs.compare(req.params.pass, result.password, (err, isMatch) => {
+      if (isMatch) {
+        return res.render("admin/delegates/getCode", { roles: result.roles });
+      }
+    })
+  })
+})
 
 require("./login")(router);
 
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,20 +1,7 @@
 var router = require("express").Router();
-var mongoose = require('mongoose');
-var bcryptjs = require('bcryptjs');
 
 router.use("/delegates", require("./delegate"));
 router.use("/votes", require("./votes"));
 router.use("/", require("./adminPage"));
-
-router.get('/test/:admin/:pass', (req, res) => {
-  console.log(req.params)
-  mongoose.model('delegates').findOne({ IdNumber: req.params.admin }, (err, result) => {
-    bcryptjs.compare(req.params.pass, result.password, (err, isMatch) => {
-      if (isMatch) {
-        res.render("admin/delegates/getCode", { roles: result.roles });
-      }
-    })
-  })
-})
 
 module.exports = router;
