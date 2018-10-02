773d3fe8899d861ccbe174829563650fd163c6dd
ducnm98 <ducnm.john98@gmail.com>
2018-10-02 19:02:40 +0700

    adding datatable

--- routes/admin/delegate/list.js
+++ routes/admin/delegate/list.js
@@ -8,16 +8,16 @@ module.exports = router => {
   router.get("/", (req, res, next) => {
     if (req.isAuthenticated()) {
       if (checkPermission(req.user.roles, TYPE_OF_ADMIN)) {
-        let pageSize = parseInt(_.get(req, 'query.pageSize', 20));
-        let skip = parseInt(_.get(req, 'query.skip', 0) * pageSize);
+        // let pageSize = parseInt(_.get(req, 'query.pageSize', 20));
+        // let skip = parseInt(_.get(req, 'query.skip', 0) * pageSize);
         mongoose.model('delegates').find().countDocuments().exec((err, total) => {
           if (err) throw err;
-          mongoose.model('delegates').find().skip(skip).limit(pageSize).exec((err, delegatesList) => {
+          mongoose.model('delegates').find().exec((err, delegatesList) => {
             if (err) throw err;
             if (delegatesList) {
-              return res.render('admin/delegates', { total, skip, pageSize, delegatesList, roles: req.user.roles })
+              return res.render('admin/delegates', { total, delegatesList, roles: req.user.roles })
             } else {
-              return res.render('admin/delegates', { total, skip, pageSize, delegatesList, roles: req.user.roles })
+              return res.render('admin/delegates', { total, delegatesList, roles: req.user.roles })
             }
           })
         })
