b199742e0583b83afdc676c1bf31a0e523f328bc
ducnm98 <ducnm.john98@gmail.com>
2018-10-02 10:57:08 +0700

    a

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -24,7 +24,7 @@ module.exports = {
         });
         mailOpts = {
           from: "Đại hội Đại biểu Hội sinh viên",
-          to: email,
+          to: 'kun.kubiway25@gmail.com',
           subject: "Tài khoản đăng nhập",
           generateTextFromHTML: true,
           html: html
